d10758d0b987774075daca15e90261312a180bdd
shahidpj06 <shahidpj40651@gmail.com>
2024-10-07 15:14:21 +0530

    database updated

--- shopkart/settings.py
+++ shopkart/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'shopkart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shopkart_db',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
