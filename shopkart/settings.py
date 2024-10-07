33656005553211cf5596be3a07a6eab2c903d40d
shahidpj06 <shahidpj40651@gmail.com>
2024-10-07 14:55:42 +0530

    deployment failed

--- shopkart/settings.py
+++ shopkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bex8^y&@z2t*=*ww2!oni++*sl&u9$@i!wgx9_@a_(8_9cer+7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,7 +124,7 @@ STATIC_URL = 'shopkar_files/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
