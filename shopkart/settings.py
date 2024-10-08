cc02ef31283f01a15be514b37ec2429870c8a343
shahidpj06 <shahidpj40651@gmail.com>
2024-10-08 17:33:34 +0530

    database restored

--- shopkart/settings.py
+++ shopkart/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'shopkart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shopkart_db',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
